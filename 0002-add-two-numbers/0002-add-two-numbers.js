551386567a85f6c268604e09fc8cbada51ec6dcb
vishal Rajput <vr987195@gmail.com>
2024-06-28 12:56:43 +0530

    Time: 106 ms (65.47%), Space: 56.5 MB (20.22%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,11 @@
+var addTwoNumbers = function(l1, l2) {
+    if (!l1 && !l2) return null;
+    else if (!l1) return l2;
+    else if (!l2) return l1;
+
+    var a = l1.val + l2.val;
+    var p = new ListNode(a % 10);
+    p.next = addTwoNumbers(l1.next, l2.next);
+    if (a >= 10) p.next = addTwoNumbers(p.next, new ListNode(1));
+    return p;
+};
