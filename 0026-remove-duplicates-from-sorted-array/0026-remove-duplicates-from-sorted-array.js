07c097793b949de23d31da0e63b0701cf0dea29d
vishal Rajput <vr987195@gmail.com>
2024-07-21 15:37:48 +0530

    Time: 59 ms (80.69%), Space: 51.7 MB (89.12%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,8 @@
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != nums[i]) 
+            nums[++i] = nums[j];
+    }
+    return ++i;
+};
